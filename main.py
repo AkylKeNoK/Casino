02babb7b7b8692dcf6052a880dc8182fbd88b479
AkylKeNoK <akylkakazn@gmail.com>
2022-06-07 13:06:53 +0400

    test

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+print("""
+      Добро пожаловать в казино
+      Испытай свою удачку :> 
+""")
+
+die = random.randint(1, 6)
+
+print
+
+#bet = int(input("Введитек число от 1 до 6: "))
+
